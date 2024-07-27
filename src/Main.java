1d94dc1c804d5d25e2fc0e5cb4deafe5ea9be88e
Sai Sri <Sai.Sri@godigit.com>
2024-07-27 11:52:54 +0530

    Added contact class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book");
+        Contact contact =new Contact("a","b","c","d","e","f","g","h");
+        System.out.println(contact.toString());
+
     }
 }
